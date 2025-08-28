a0c3bde54a839526f621325b1bab6ad41e1ad1ca
Evans Elabo <ellaboevans@gmail.com>
2025-08-28 14:44:26 +0000

    Update ESLint configuration: add rules to disable specific React linting warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,10 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/react-in-jsx-scope": "off",
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
